6f47acdf5880d0f6cff1a99f84b873775d0aa1a4
Liam Burke <hello@burk.ie>
2014-09-14 15:19:55 +0100

    Tweet that update
    Allowing you specify a via twitter account.

---
+++ tweet_that.php
@@ -0,0 +1,39 @@
+<?php  
+/*
+ Plugin Name: Tweet That
+ Plugin URI: http://burkie.com
+ Description: Tweets in Posts
+ Version: 0.6
+ Author: Liam Burke
+ Author URI: burkie.com
+ */
+
+function tweet_shortcode($atts, $content = null) { 
+	extract(shortcode_atts(array("bird" => 'no', "via" => ''.get_bloginfo('name').'', "hashtag" => ''), $atts)); ?>
+	
+	<script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
+	
+	<style>
+		.bird {
+			background: url('<?php bloginfo('url'); ?>/wp-content/plugins/tweet-that/img/bird.png') no-repeat;
+			text-indent: -9999px;
+			margin: 4px 0 0 4px;		
+			width: 20px;
+			height: 16px;
+			display: inline-block;
+		}
+	</style>
+	
+	<?php
+		
+	if ( $bird == 'no' ) {
+		return '<a href="https://twitter.com/intent/tweet?status='.$content = strip_tags($content,'').' '.get_permalink('').' via '.$via.' " target="_blank">'. $content .'</a>';
+		
+		} else {
+		return '<a href="https://twitter.com/intent/tweet?status='.$content = strip_tags($content,'').' '.get_permalink('').' via '.$via.' " target="_blank">'. $content. '<div class=\'bird\'>' .$bird.'</div></a>' ;
+	}
+}
+
+add_shortcode( 'tweet', 'tweet_shortcode' );
+
+?>
